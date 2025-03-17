285f9dfca27c82fe9af83f74ca0c2fab9473d528
anish77 <45210460+anish77@users.noreply.github.com>
2025-03-17 15:27:36 +0100

    - to delete

--- lib/widgets/tab_bar.dart
+++ lib/widgets/tab_bar.dart
@@ -1,5 +1,6 @@
 import 'package:farmagest/data/constants.dart';
 import 'package:farmagest/provider/tcp_connection.dart';
+import 'package:farmagest/screens/agenda2.dart';
 import 'package:farmagest/screens/agenda_page.dart';
 import 'package:farmagest/screens/clienti_page.dart';
 import 'package:farmagest/screens/login_page.dart';
@@ -64,7 +65,8 @@ class _TabBarWidgetState extends ConsumerState<TabBarWidget> {
         index: _currentIndex,
         children: [
           Center(child: ClientiPage()),
-          Center(child: AgendaPage()),
+          //Center(child: AgendaPage()),
+          Center(child: CalendarScreen()),
           Center(),
         ],
       ),
